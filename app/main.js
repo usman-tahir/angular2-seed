b03c8b7239e3b2c3b94b48afe57834a40cba48c3
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-13 12:48:55 -0400

    removed files built upon the npm start command

--- app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var Module_1 = require("./Module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(Module_1.AppModule);
-//# sourceMappingURL=main.js.map
