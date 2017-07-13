d880cdd3b2c476ee8222b908737e22345ed5c398
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-13 12:44:18 -0400

    added some structural typescript files for loading a basic application

---
+++ app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var Module_1 = require("./Module");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(Module_1.AppModule);
+//# sourceMappingURL=main.js.map
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./Module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
